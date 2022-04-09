103fdf9e47056b22a0ad5298343d1a735b018de0
Shaikh Islam <shaikhalvee@gmail.com>
2022-04-09 06:46:54 -0400

    test class

---
+++ project/Temp.java
@@ -0,0 +1,23 @@
+package project;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Temp {
+	public static void main(String[] args) {
+		Node a = new Node('A', 5);
+		Node b = new Node('B', 3);
+		Node c = new Node('C', 2);
+		Node d = new Node('D', 7);
+		Node e = new Node('E', 9);
+		PriorityQueue<Node> nodePriorityQueue = new PriorityQueue<>();
+		nodePriorityQueue.add(a);
+		nodePriorityQueue.add(b);
+		nodePriorityQueue.add(c);
+		nodePriorityQueue.add(d);
+		nodePriorityQueue.add(e);
+		System.out.println(nodePriorityQueue);
+
+	}
+}
