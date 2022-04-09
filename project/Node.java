f6a9f4ea328cfce44ae78f9ffecf9823292ee581
Shaikh Islam <shaikhalvee@gmail.com>
2022-04-09 06:46:30 -0400

    comparable added for priority queue

--- project/Node.java
+++ project/Node.java
@@ -1,6 +1,6 @@
 package project;
 
-public class Node {
+public class Node implements Comparable<Node> {
 	Character vertex;
 	Integer weight;
 
@@ -10,5 +10,10 @@ public class Node {
 	Node(Character vertex, Integer weight) {
 		this.vertex = vertex;
 		this.weight = weight;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		return this.weight.compareTo(o.weight);
 	}
 }
