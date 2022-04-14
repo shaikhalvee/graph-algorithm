3526c62e45caf85999bc841aaec84fe77a93569e
Shaikh Islam <shaikhalvee@gmail.com>
2022-04-14 15:48:14 -0400

    input out structure updated

--- project/GenerateInput.java
+++ project/GenerateInput.java
@@ -1,13 +1,14 @@
 package project;
 
-import java.io.*;
-import java.util.Random;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class GenerateInput {
 	public static void main(String[] args) throws IOException {
-		final String outputFile = "src/input1.txt";
+		final String outputFile = "src/output.txt";
 		final String inputFile = "src/input1.txt";
 		FileWriter fileWriter;
 		FileReader fileReader;
@@ -21,15 +22,25 @@ public class GenerateInput {
 		}
 		Scanner scanner = new Scanner(fileReader);
 		int loop = scanner.nextInt();
+		bufferedWriter.write(loop + "\n");
+//		bufferedWriter.newLine();
 		for (int i = 0; i < loop; i++) {
 			int vertex, edge;
 			vertex = scanner.nextInt();
 			edge = scanner.nextInt();
 			bufferedWriter.write(vertex + " " + edge + "\n");
 			for (int j = 0; j < edge; j++) {
-
+				int node1, node2, weight;
+				node1 = scanner.nextInt();
+				node2 = scanner.nextInt();
+				weight = scanner.nextInt();
+				char node_1 = (char) (node1 + 'A');
+				char node_2 = (char) (node2 + 'A');
+				bufferedWriter.write(node_1 + " " + node_2 + " " + weight + "\n");
 			}
 		}
+		bufferedWriter.close();
+		scanner.close();
 //		for (int i = 0; i < 2; i++) {
 //			Integer vertexU = ThreadLocalRandom.current().nextInt(10, 101);
 //			Integer vertexD = ThreadLocalRandom.current().nextInt(11, 101);
